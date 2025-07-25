f0d771985cb18cf9b7e6d61803f3c23cc131e44f
Andrés <alumno.254958@ies-azarquiel.es>
2025-07-25 23:09:31 +0200

    feat: updated python script to pass user cookies (optional), relay on default cookies or None

--- pkg/pyConverter/main.py
+++ pkg/pyConverter/main.py
@@ -1,65 +1,136 @@
 import yt_dlp
 import sys
-import json
 import os
 import subprocess
+import argparse
+
+
+# Returns the path of the cookie file to use if exists
+def get_cookie_file_path(cookies_path: str | None) -> str | None:
+    """
+    Devuelve la ruta del archivo de cookies si existe.
+    Si no existe ninguno, devuelve None.
+    """
+    if cookies_path and os.path.exists(cookies_path):
+        print("Usando cookies %s" % cookies_path)
+        return cookies_path
+
+    default_path = "./cookies.txt"
+    if os.path.exists(default_path):
+        print("Usando cookies %s" % cookies_path)
+        return default_path
+
+    return None
+
+
+# Check if user cookie path exists, if not, check if default cookie path exists.
+def check_if_cookies_file_is_present(cookies_path: str | None) -> bool:
+    """
+    Verifica si existe un archivo de cookies.
+    Prioriza el path proporcionado por el usuario, luego el archivo por defecto.
+    """
+    default_path = "./cookies.txt"
+
+    if cookies_path and os.path.exists(cookies_path):
+        return True
+
+    return os.path.exists(default_path)
+
 
 def convert_m4a_to_mp3(m4a_path: str, mp3_path: str) -> str:
     try:
         # Use ffmpeg to convert M4A to MP3
-        subprocess.run([
-            'ffmpeg', '-i', m4a_path, '-codec:a', 'libmp3lame', '-q:a', '2', mp3_path
-        ], check=True)
+        subprocess.run(
+            ["ffmpeg", "-i", m4a_path, "-codec:a", "libmp3lame", "-q:a", "2", mp3_path],
+            check=True,
+        )
         return mp3_path
     except subprocess.CalledProcessError as e:
         print(f"Error converting M4A to MP3: {e}")
         return ""
-    
-def convert_to_audio(youtube_url: str, output_path: str, youtube_id: str) -> str:
-    ydl_opts = {
-    'format': 'm4a/bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'm4a',
-    }],
-    'outtmpl': output_path + '/%(id)s.%(ext)s',
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        error_code = ydl.download(youtube_url)
-        return output_path + f'/{youtube_id}.m4a'
 
-def get_video_available_resolutions(youtube_url: str) -> list[str]:
-    ydl_opts = {
-        'quiet': True,
-    }
+
+def convert_to_audio(
+    output_path: str, youtube_id: str, cookies_path: str = None
+) -> str:
+    if check_if_cookies_file_is_present(cookies_path):
+        ydl_opts = {
+            "format": "m4a/bestaudio/best",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "m4a",
+                }
+            ],
+            "outtmpl": output_path + "/%(id)s.%(ext)s",
+            "cookiefile": get_cookie_file_path(cookies_path=cookies_path),
+        }
+    else:
+        ydl_opts = {
+            "format": "m4a/bestaudio/best",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "m4a",
+                }
+            ],
+            "outtmpl": output_path + "/%(id)s.%(ext)s",
+        }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        return output_path + f"/{youtube_id}.m4a"
+
+
+def get_video_available_resolutions(youtube_url: str, cookies_path: str = None) -> list[str]:
+    if check_if_cookies_file_is_present(cookies_path):
+        ydl_opts = {
+            "quiet": True,
+            "cookiefile": get_cookie_file_path(cookies_path=cookies_path),
+        }
+    else:
+        ydl_opts = {
+            "quiet": True,
+        }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(youtube_url, download=False)
-        streams = info.get('formats', [])
+        streams = info.get("formats", [])
         resolutions = set()
 
         for stream in streams:
-            if stream.get('vcodec') not in [None, 'none']:
-                height = stream.get('height')
+            if stream.get("vcodec") not in [None, "none"]:
+                height = stream.get("height")
                 if height:
                     resolutions.add(f"{height}p")
 
-        return sorted(resolutions, key=lambda x: int(x.replace('p', '')))
+        return sorted(resolutions, key=lambda x: int(x.replace("p", "")))
 
-def convert_to_video(youtube_url: str, resolution: str, output_path: str) -> str:
+
+def convert_to_video(youtube_url: str, resolution: str, output_path: str, cookies_path: str = None) -> str:
     available_resolutions = get_video_available_resolutions(youtube_url)
-    
+
     if resolution not in available_resolutions:
-        print(f"❌ Resolution {resolution} not available. Available resolutions: {available_resolutions}")
+        print(
+            f"❌ Resolution {resolution} not available. Available resolutions: {available_resolutions}"
+        )
         return ""
-    
+
     print(f"⏳ Downloading video {youtube_url} with resolution {resolution}...")
-    ydl_opts = {
-        'format': f'bestvideo[height={resolution[:-1]}]+bestaudio/best',
-        'outtmpl': output_path + '/%(id)s-%(resolution)s.%(ext)s',
-        'merge_output_format': 'mp4',
-    }
-    
+
+    if check_if_cookies_file_is_present(cookies_path):
+        ydl_opts = {
+            "format": f"bestvideo[height={resolution[:-1]}]+bestaudio/best",
+            "outtmpl": output_path + "/%(id)s-%(resolution)s.%(ext)s",
+            "merge_output_format": "mp4",
+            "cookiefile": get_cookie_file_path(cookies_path=cookies_path)
+        }
+    else:
+        ydl_opts = {
+            "format": f"bestvideo[height={resolution[:-1]}]+bestaudio/best",
+            "outtmpl": output_path + "/%(id)s-%(resolution)s.%(ext)s",
+            "merge_output_format": "mp4",
+        }
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
             info_dict = ydl.extract_info(youtube_url, download=True)
@@ -69,63 +140,71 @@ def convert_to_video(youtube_url: str, resolution: str, output_path: str) -> str
             print(f"Error downloading video: {e}")
             return ""
 
+
 def video_to_youtube_url(video_id: str) -> str:
     return f"https://www.youtube.com/watch?v={video_id}"
+
 
 def delete_repeated_resolutions(resolutions: list[str]) -> list[str]:
     return list(set(resolutions))
 
+
 def check_if_path_is_valid_and_absolute(path: str) -> bool:
     return os.path.exists(path) and os.path.isabs(path)
 
-if __name__ == "__main__":
 
-    if len(sys.argv) < 4:
-        print("Please provide a video ID, convert to format (audio/video) and output path")
-        exit(1)
+def main():
+    parser = argparse.ArgumentParser(
+        description="Convertidor de videos de YouTube a audio/video con soporte opcional para cookies"
+    )
 
-    # Get Video ID from Parameter
-    video_id = sys.argv[1]
+    parser.add_argument("video_id", help="ID del video de YouTube")
+    parser.add_argument(
+        "convert_to",
+        choices=["audio", "video"],
+        help="Formato de conversión: audio o video",
+    )
+    parser.add_argument(
+        "output_path", help="Ruta absoluta donde guardar el archivo de salida"
+    )
 
-    # Get If you want to convert to audio or video
-    convert_to = sys.argv[2] 
+    parser.add_argument(
+        "--cookies", help="Ruta al archivo cookies.txt (opcional)", default=None
+    )
+    parser.add_argument(
+        "--resolution",
+        help="Resolución deseada para video (opcional si convert_to=video)",
+    )
 
-    output_path = sys.argv[3]
+    args = parser.parse_args()
 
-    if not check_if_path_is_valid_and_absolute(output_path):
-        print("Please provide a valid and absolute output path")
-        exit(1)
+    # Validaciones básicas
+    if not check_if_path_is_valid_and_absolute(args.output_path):
+        print("❌ Por favor proporciona una ruta absoluta válida para la salida.")
+        sys.exit(1)
 
-    if video_id is None:
-        print("Please provide a video ID")
-        exit(1)
+    video_url = video_to_youtube_url(args.video_id)
 
-    if convert_to is None or convert_to not in ["audio", "video"]:
-        print("Please provide a convert to format (audio/video)")
-        exit(1)
-
-    if output_path is None:
-        print("Please provide a output path")
-        exit(1)
-
-    # Get Video URL
-    video_url = video_to_youtube_url(video_id)
-
-    if convert_to == "audio":
-        path = convert_to_audio(video_url, output_path, video_id)
-        mp3path = convert_m4a_to_mp3(path, output_path + f'/{video_id}.mp3')
+    if args.convert_to == "audio":
+        path = convert_to_audio(args.output_path, args.video_id, args.cookies)
+        mp3path = convert_m4a_to_mp3(
+            path, os.path.join(args.output_path, f"{args.video_id}.mp3")
+        )
         os.remove(path)
         print(mp3path)
-        exit(0)
-    elif convert_to == "video":
-        try:
-            resolution = sys.argv[4]
-        except IndexError:
-            resolution = None
-        if resolution is None:
-            resolutions = get_video_available_resolutions(video_url)
-            print(resolutions)
-            exit(0)
-        path = convert_to_video(video_url, resolution, output_path)
+        sys.exit(0)
+
+    elif args.convert_to == "video":
+        if not args.resolution:
+            resolutions = get_video_available_resolutions(video_url, args.cookies)
+            print("Resoluciones disponibles:", resolutions)
+            sys.exit(0)
+        path = convert_to_video(
+            video_url, args.resolution, args.output_path, args.cookies
+        )
         print(path)
-        exit(0)
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
